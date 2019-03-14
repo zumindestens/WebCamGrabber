3057b61d3456871efb98a78e2f1c2ec92b243089
tea <60609@studmail.htw-aalen.de>
2019-03-14 20:11:35 +0100

    add gitignore and logging, major refactoring

--- main.py
+++ main.py
@@ -1,36 +1,81 @@
-import time
 from datetime import datetime
 import urllib.request
 import os
 import sys
 import pytz
 import requests
+import traceback
 from GPSPhoto import gpsphoto
 import piexif
 from PIL import Image
 
+# example call: python3 main.py "http://example.de" Nordyl /home/tea/ marinetraffic id
 
 
-
-# example call: python3 main.py "http://example.de" Nordyl /home/tea/
-
-def new_folder():
-    return baseFolder + shipName + "-" + datetime.now(timeZone).strftime("%d-%m-%Y")
+class NoGPSInfoException(Exception):
+    pass
 
 
+def log(text, exception):
+    assert isinstance(exception,Exception)
+    with open("log", "a+") as file:
+        file.write(datetime.now(timeZone).strftime("%d-%m-%Y-%h-%m-%s") + ": \n")
+        file.write(text + "\n")
+        file.write(traceback.format_exception_only(exception))
+        file.write("\n")
+
+
+def createFolderIfPossible():
+    folder = os.path.join(baseFolder, shipName + "-" + datetime.now(timeZone).strftime("%d-%m-%Y"))
+    try:
+        os.mkdir(folder)
+    except FileExistsError:
+        # expected behavior should only not reach this when there is a new day
+        pass
+    return folder
+
+
+# setting gps coordinates of an existing image, copies it into the correct place, might raise NOGGSInfoException
+def set_gps(filePath):
+    # get GPS coordinates
+    try:
+        resp = requests.get("https://www.marinetraffic.com/map/getvesseljson/shipid:" + marineTrafficId,
+                            headers=headers)
+    except Exception as e:
+        log("marinetraffic not reached",e)
+        raise NoGPSInfoException
+    longitude = resp.json()["LON"]
+    latitude = resp.json()["LAT"]
+
+    # set gps in exif
+    photo = gpsphoto.GPSPhoto(filePath)
+    info = gpsphoto.GPSInfo((float(latitude), float(longitude)))
+    photo.modGPSData(info, filePath)
+
+    # remove broken gps exif data
+    brokenImg = Image.open(filePath)
+    exif_dict = piexif.load(brokenImg.info["exif"])
+    exif_dict["GPS"].pop(piexif.GPSIFD.GPSProcessingMethod, None)
+    exif_bytes = piexif.dump(exif_dict)
+
+    folder = createFolderIfPossible()
+
+    try:
+        filePath = os.path.join(folder, shipName + datetime.now(timeZone).strftime("-%H:%M") + ".jpg")
+        brokenImg.save(filePath, exif=exif_bytes)
+    except Exception as e:
+        log("failed to save manipulated exif data", e)
+        raise NoGPSInfoException
+
+
+# initiallize values from call and time, open log
 timeZone = pytz.timezone('CET')
 url = sys.argv[1]
 shipName = sys.argv[2]
 baseFolder = sys.argv[3]
 marineTrafficId = sys.argv[4]
-day = datetime.now(timeZone).day
-folder = new_folder()
 
-try:
-    os.mkdir(folder)
-except Exception:
-    print("nothing todo as prob folder just already exists")
-
+# headers used for marinetraffic request
 headers = {
 "Host": "www.marinetraffic.com",
 "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0",
@@ -45,46 +90,25 @@ headers = {
 "Cache-Control": "no-cache",
 "TE": "Trailers"}
 
-
-while True:
-    if datetime.now(timeZone).day != day:
-        day = datetime.now(timeZone).day
-        folder = new_folder()
-        os.mkdir(folder)
-
-    time.sleep(5 * 60)
-
-    try:
-        filePath = folder + "/" + shipName + datetime.now(timeZone).strftime("-%H:%M") + ".jpg"
-        urllib.request.urlretrieve(url, filePath)
-
-        try:
-            # get GPS coordinates
-            resp = requests.get("https://www.marinetraffic.com/map/getvesseljson/shipid:" + marineTrafficId,
-                                headers=headers)
-            longitude = resp.json()["LON"]
-            latitude = resp.json()["LAT"]
-
-            # set gps in exif
-            photo = gpsphoto.GPSPhoto(filePath)
-            info = gpsphoto.GPSInfo((float(latitude), float(longitude)))
-            photo.modGPSData(info, filePath)
-
-            # remove broken gps exif data
-            brokenImg = Image.open(filePath)
-            exif_dict = piexif.load(brokenImg.info["exif"])
-            exif_dict["GPS"].pop(piexif.GPSIFD.GPSProcessingMethod, None)
-            exif_bytes = piexif.dump(exif_dict)
-            brokenImg.save(filePath, exif=exif_bytes)
-        except Exception:
-            # do nothing probably just a timeout
-            print("")
-
-    except Exception:
-        # do nothing probably just a timeout
-        print("")
+try:
+    # get the latest image
+    filePath = os.path.join(baseFolder, "/current.jpg")
+    urllib.request.urlretrieve(url, filePath)
+except FileExistsError as f:
+    log("Couldn't create new current.jpg file", f)
+    sys.exit(2)
+except Exception as e:
+    log("Failed to receive webcam image", e)
+    sys.exit(1)
 
 
+try:
+    set_gps(filePath)
+except NoGPSInfoException:
+    folder = createFolderIfPossible()
+    newFilePath = os.path.join(folder, shipName + datetime.now(timeZone).strftime("-%H:%M") + ".jpg")
+    os.rename(filePath, newFilePath)
+except Exception as e:
+    log("Unknown Exception setting gps info", e)
 
-
-
+sys.exit(0)
